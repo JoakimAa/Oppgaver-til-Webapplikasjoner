d5d6b112c844ad08ceba3b711ea464598c0ba449
JoakimAa <joakimsa@hiof.no>
2020-11-17 22:10:49 +0100

    Added heading

--- Oppgave_Leksjon_11/client/src/pages/LogIn.jsx
+++ Oppgave_Leksjon_11/client/src/pages/LogIn.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Heading } from '@chakra-ui/core';
 import LoginForm from '../components/LogInForm';
 
 const LogIn = ({ setcurrentUser }) => (
   <section>
+    <Heading mb={2} as="h1" size="md">
+      Logg inn
+    </Heading>
     <LoginForm setcurrentUser={setcurrentUser} />
   </section>
 );
